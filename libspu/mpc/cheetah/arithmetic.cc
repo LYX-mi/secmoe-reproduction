928efb6cd5e94dd7c6d7188957d50bf47d5eac02
lianhua <zhoujinjin.zjj@antgroup.com>
2025-12-16 02:20:23 +0000

    fix tmp

--- libspu/mpc/cheetah/arithmetic.cc
+++ libspu/mpc/cheetah/arithmetic.cc
@@ -530,6 +530,7 @@ void BatchMatMulAA::evaluate(KernelEvalContext* ctx) const {
   auto xs = lhs.shape();
   auto ys = rhs.shape();
   SPU_ENFORCE(xs.ndim() == ys.ndim(), "ndim mismatch: lhs={}, rhs={}", xs, ys);
+  SPU_ENFORCE(xs.ndim() == 3, "ndim mismatch: lhs={}, expected=3", xs);
   SPU_ENFORCE(xs[0] == ys[0], "batch mismatch: lhs={}, rhs={}", xs, ys);
   SPU_ENFORCE(xs[2] == ys[1], "shape mismatch: lhs={}, rhs={}", xs, ys);
   ctx->pushOutput(WrapValue(proc(ctx, lhs.data(), rhs.data())));
